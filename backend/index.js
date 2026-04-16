9a43c77f788f277802cba288afd2e72ef964c264
Bhogendra <bhogendra488@gmail.com>
2026-04-16 09:42:48 +0530

    Updated register.jsx file

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const app=express()
+const cors=require("cors")
+require("dotenv").config()
+const authRoutes=require("./routes/authRoutes")
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log("DB connected")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+
+app.get("/",(req,res)=>{
+    res.json({message:"server is running"})
+})
+app.use("/api/auth",authRoutes)
+
+app.listen(5000,()=>{
+    console.log("Server is running on 5000 port")
+})
